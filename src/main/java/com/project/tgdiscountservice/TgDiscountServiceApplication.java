e99d63f0353f145a02671b6470d2f51ae137f427
evgeniy-panferov <418vqftn8ApZ2J>
2021-12-18 21:44:52 +0300

    service template

---
+++ src/main/java/com/project/tgdiscountservice/TgDiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.tgdiscountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgDiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgDiscountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tgdiscountservice/TgDiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tgdiscountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgDiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
