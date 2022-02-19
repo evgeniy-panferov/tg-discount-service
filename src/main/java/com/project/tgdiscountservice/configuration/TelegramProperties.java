867be8e1f89ce5455e7830bc1b8de75380fc718d
evgeniy-panferov <418vqftn8ApZ2J>
2022-02-19 10:40:42 +0300

    moved to environment

--- src/main/java/com/project/tgdiscountservice/configuration/TelegramProperties.java
+++ src/main/java/com/project/tgdiscountservice/configuration/TelegramProperties.java
@@ -1,14 +1,16 @@
 package com.project.tgdiscountservice.configuration;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "telegram")
 @Getter
+@Setter
 public class TelegramProperties {
 
-    private String botUsername = "discount_service_bot";
+    private String botUsername;
 
-    private String botToken = "5082235523:AAGHWkaNSh1rrCz8Kr9rN8rP08bkZrhyLQk";
+    private String botToken;
 
 }
